c7bc7af3e59d20bee33af65c8503c982fe9e1e11
Fachri Hawari <fachri.hawari@gmail.com>
2024-11-22 06:41:16 +0700

    clean up umami related code and bump new bun version

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv {
-  UMAMI_WEBSITE_ID: string
-  UMAMI_ENDPOINT: string
-  UMAMI_USERNAME: string
-  UMAMI_PASSWORD: string
-}
+interface ImportMetaEnv {}
